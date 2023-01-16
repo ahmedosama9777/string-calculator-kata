0137d8c686ee79b92194f230795afad3e0637fbf
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-16 20:03:23 +0200

    Add validation of spearator at end

--- string_calculator.py
+++ string_calculator.py
@@ -1,12 +1,23 @@
 import re
 
-class StringCalculator():
+
+class StringCalculator:
     def add(self, string: str) -> int:
         if string == "":
             return 0
         elif len(string) == 1:
             return int(string)
         else:
-            numbers_list = re.split('[, \n]', string)
-            numbers_list = [int(num) for num in numbers_list]
+            numbers_list = self._process_input_string(string)
+
             return sum(numbers_list)
+
+    def _process_input_string(self, string):
+        numbers_list = re.split("[, \n]", string)
+
+        if numbers_list[-1] == "":
+            raise ValueError("Separators are not allowed at the end of string")
+
+        numbers_list = [int(num) for num in numbers_list]
+
+        return numbers_list
--- string_calculator_test.py
+++ string_calculator_test.py
@@ -2,22 +2,30 @@ from unittest import TestCase
 
 from string_calculator import StringCalculator
 
+
 class TestStringCalculator(TestCase):
     def setUp(self) -> None:
         self.calculator = StringCalculator()
 
     def test_empty_string_return_zero(self):
         self.assertEqual(self.calculator.add(""), 0)
-    
+
     def test_one_number_string(self):
         self.assertEqual(self.calculator.add("1"), 1)
-    
+
     def test_two_numbers_separated_by_comma(self):
         self.assertEqual(self.calculator.add("1,2"), 3)
-    
+
     def test_more_than_two_numbers(self):
         self.assertEqual(self.calculator.add("1,2,3"), 6)
 
     def test_new_line_as_separator(self):
         self.assertEqual(self.calculator.add("1,2\n3"), 6)
-        
+
+    def test_raises_error_separator_at_end(self):
+        with self.assertRaises(ValueError) as e:
+            self.calculator.add("1,2,")
+
+        self.assertEqual(
+            str(e.exception), "Separators are not allowed at the end of string"
+        )
