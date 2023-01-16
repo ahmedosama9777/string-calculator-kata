ce203446d902d4247d0b711150c390dc20a85fd4
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-16 19:40:55 +0200

    Add adding two numbers

--- string_calculator.py
+++ string_calculator.py
@@ -2,5 +2,8 @@ class StringCalculator():
     def add(self, string: str) -> int:
         if string == "":
             return 0
-        else:
+        elif len(string) == 1:
             return int(string)
+        else:
+            number1, number2 = string.split(",")
+            return int(number1) + int(number2)
--- string_calculator_test.py
+++ string_calculator_test.py
@@ -7,7 +7,10 @@ class TestStringCalculator(TestCase):
         self.calculator = StringCalculator()
 
     def test_empty_string_return_zero(self):
-        self.assertEqual(self.calculator .add(""), 0)
+        self.assertEqual(self.calculator.add(""), 0)
     
     def test_one_number_string(self):
-        self.assertEqual(self.calculator .add("1"), 1)
+        self.assertEqual(self.calculator.add("1"), 1)
+    
+    def test_two_numbers_separated_by_comma(self):
+        self.assertEqual(self.calculator.add("1,2"), 3)
